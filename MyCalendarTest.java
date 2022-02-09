00272cfe74dd496b45c9d3934f65470839449727
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-02-09 14:53:05 +0900

    test: MyCalendar getTodayTest생성

---
+++ MyCalendarTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class MyCalendarTest {
+
+    @Test
+    void getTodayTest(){
+        MyCalendar mycalendar = new MyCalendar();
+        mycalendar.getToday();
+    }
+
+
+}
